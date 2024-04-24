bca4a8484fb66ad3c5d608e07221396f9b0cb41e
pt-estimate <christopherwright@protonmail.com>
2024-04-24 16:43:31 -0700

    adding in model logic

--- merchantizer/model.py
+++ merchantizer/model.py
@@ -32,21 +32,17 @@ class MerchantModelFlow(FlowSpec):
         cleaned_df = run.data.cleaned_df
         # TODO: Leverage gpu acceleration to improve sample size
         sampled_df = cleaned_df.sample(100000)
-        sampled_test_df = cleaned_df.sample(100000)
         sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
-        embedding_df = pd.concat([sampled_df, sampled_test_df])
         embeddings = sentence_model.encode(sampled_df["cleaned_description"] \
                                            .tolist(), show_progress_bar=False)
 
         # We define a number of merchants that we know are in the documents
-        zeroshot_topic_list = ["square*","cashapp*","toast*","non-merchant",
-                               "doordash*","target","walmart", "mcdonalds",
-                               "apple", "uber", "google", "amazon", "shell",
-                               "circlek", "7-eleven", "dollargeneral",
-                               "dollartree", "lyft", "tacobell", "exxon",
-                               "starbucks", "chevron", "wendys", "365market",
-                               "wawa", "familydollar", "speedway"]
-
+        zeroshot_topic_list = ["square*","cashapp*","toast*","doordash*",
+                               "target","walmart","mcdonalds","apple","uber",
+                               "google","amazon","shell","circle-k","7-eleven",
+                               "dollargeneral","dollartree","familydollar",
+                               "lyft","tacobell","exxon","starbucks","chevron",
+                               "wendys","365-market"]
 
         # We fit our model using the zero-shot merchants
         # and we define a minimum similarity. For each document,
@@ -61,27 +57,16 @@ class MerchantModelFlow(FlowSpec):
         representation_model=KeyBERTInspired())
 
         topics, _ = topic_model.fit_transform(sampled_df \
-                                              ["cleaned_description"] \
-                                              .tolist(), embeddings)
-
-        test_topics, _ = topic_model.transform(sampled_test_df \
-                                              ["cleaned_description"] \
-                                              .tolist(), embeddings)
+                ["cleaned_description"].tolist())
+        print(topic_model.get_topic_info().head(10))
+        self.topic_info_df = topic_model.get_topic_info().head(30)
 
         # Append predictions to fit set
         sampled_df["predicted_topic"] = topics
         sampled_df["predicted_merchant"] = sampled_df["predicted_topic"] \
         .apply(lambda _:topic_model.get_topic_info(_)["Name"])
 
-        #Append predictions to test set
-        sampled_test_df["predicted_topic"] = test_topics
-        sampled_test_df["predicted_merchant"] = sampled_test_df \
-                ["predicted_topic"].apply(lambda _: \
-                topic_model.get_topic_info(_)["Name"])
-
         self.fit_prediction_df = sampled_df
-        self.transform_prediction_df = sampled_test_df
-        self.topic_info_df = topic_model.get_topic_info()
         self.next(self.end)
 
     @step
