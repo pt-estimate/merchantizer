c4213a7540ab3e72cf515b0ab58129f49a58b8cb
pt-estimate <christopherwright@protonmail.com>
2024-04-24 14:17:48 -0700

    adding data load logic

---
+++ README.md
@@ -0,0 +1,36 @@
+## merchantizer
+Given a noisy set of transaction descriptions, leverage ML to predict a given transactions merchant
+
+### Project Structure
+
+There are three folders here, each with a .py file where the work is carried out, and a jupyter notebook. I recommend walking through the notebooks first as commentary on decisions made. Then use the python code to dig in more.
+
+* load - Data download and initial data discovery
+* clean - Data cleaning in preparation for modeling
+* model - Data prep in support of modeling and merchant categorization
+
+#### Prerequisites
+homebrew
+python 3
+pipx
+poetry
+
+#### Installing
+```zsh
+brew install pipx
+pipx install poetry
+```
+
+Clone this github repo
+
+From within the downloaded repo, run the following to load the data for exploration.
+
+```zsh
+poetry shell
+python merchantizer/load/load.py run
+jupyter-notebook merchantizer/load_discussion.ipynb
+```
+
+### Built With
+* [Poetry](https://python-poetry.org) - Dependency Management & Packaging
+* [Metaflow](https://metaflow.org) - Machine Learning Framework
---
+++ merchantizer/load/load.py
@@ -0,0 +1,52 @@
+from metaflow import FlowSpec, step, IncludeFile
+
+RAW_URL = r"https://drive.usercontent.google.com/download?id=1ipLgd_ZzAjRXTCfoIcpCUCA_seqqG_iY&confirm=t&uuid=a208ebfc-464b-4e4f-a3f5-d3d2ae7b6c7b&at=APZUnTXnsGYGd_lVT2u5p52PGvw1:1713713272496"
+FILENAME = "10mil_random.csv"
+
+
+class TransactionLoadFlow(FlowSpec):
+    """
+    A flow to download the transaction data..
+
+    This flow performs the following steps:
+    1) Attempt to download the data from a local file
+    2) Otherwise download data from remote url
+    3) Prints shape of downloaded data
+    """
+
+    @step
+    def start(self):
+        """
+        The start step:
+        1) Checks if a local copy exists and downloads it if not
+        2) Otherwise loads the local transaction data into dataframe
+
+        """
+        import io
+        import os
+        import pandas as pd
+        import requests
+        import zipfile
+
+        if not os.path.isfile(FILENAME):
+            print(f"Reading data from {RAW_URL}")
+            r = requests.get(RAW_URL)
+            z = zipfile.ZipFile(io.BytesIO(r.content))
+            z.extractall()
+
+        self.df = pd.read_csv(FILENAME)
+        self.next(self.end)
+
+    @step
+    def end(self):
+        """
+        The end step:
+        1) Prints out the shape of the dataframe
+
+        """
+        print(f" The data has {self.df.shape[0]} records (rows)" + \
+        f" and {self.df.shape[1]} fields (columns).")
+
+
+if __name__ == "__main__":
+    TransactionLoadFlow()
